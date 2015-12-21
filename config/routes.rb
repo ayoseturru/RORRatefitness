81e848922d5a562488e81ce02bcac9cd527c7673
ayoseturru <ayose_gladiator@hotmail.com>
2015-12-21 18:02:44 +0000

    For c9

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root "welcome#index"
   get ':controller(/:action(/:id(.:format)))'
   
   # The priority is based upon order of creation: first created -> highest priority.
