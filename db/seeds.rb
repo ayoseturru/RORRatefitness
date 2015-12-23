8c8c8b25fecf5d310dba79010a103b0acbb965b9
ayoseturru <ayose_gladiator@hotmail.com>
2015-12-23 20:30:12 +0000

    Sessions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.create email: "test@test.com", username: "test", name: "test", password: "test", password_confirmation: "test"
